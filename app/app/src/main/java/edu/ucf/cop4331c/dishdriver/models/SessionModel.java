1d68fa8d4f8697f440720d72bfa71049910579b9
rebeca amaya <rebeca amaya>
2017-03-14 17:18:53 -0400

    fixed to model

--- app/app/src/main/java/edu/ucf/cop4331c/dishdriver/models/SessionModel.java
+++ app/app/src/main/java/edu/ucf/cop4331c/dishdriver/models/SessionModel.java
@@ -6,15 +6,15 @@ package edu.ucf.cop4331c.dishdriver.models;
 
 public class SessionModel {
 
-    private static User user;
-    private static Position position;
-    private static Restaurant restaurant;
+    private static UserModel user;
+    private static PositionModel position;
+    private static RestaurantModel restaurant;
 
     public static Call<String> login(String username, String password) throws UnsupportedOperationException{ throw new UnsupportedOperationException; }
     public static Call<Boolean> login(String token) throws UnsupportedOperationException{ throw new UnsupportedOperationException; }
     public static Call logout() throws UnsupportedOperationException{ throw new UnsupportedOperationException; }
 
-    public static User currentUser(){
+    public static UserModel currentUser(){
         return user;
     }
     public static PositionModel currentPosition(){
