82b36718fe745acf9eec588bbd9e24ad3d62c427
Vivienne Do <viviennedo94@gmail.com>
2017-03-14 20:15:55 -0400

    changed text type

--- app/app/src/main/java/edu/ucf/cop4331c/dishdriver/SignUpActivity.java
+++ app/app/src/main/java/edu/ucf/cop4331c/dishdriver/SignUpActivity.java
@@ -16,6 +16,5 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
-
     }
 }
