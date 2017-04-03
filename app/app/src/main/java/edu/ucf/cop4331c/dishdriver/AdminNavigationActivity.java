fa7ec47134f4a43f0ce1eed7718dcec58aa15ecb
Gramajo <u64j3u@BBAAviation.net>
2017-04-03 17:13:49 -0400

    ginished admin splash page buttons

--- app/app/src/main/java/edu/ucf/cop4331c/dishdriver/AdminNavigationActivity.java
+++ app/app/src/main/java/edu/ucf/cop4331c/dishdriver/AdminNavigationActivity.java
@@ -10,4 +10,29 @@ public class AdminNavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_navigation);
     }
+
+    @OnClick(R.id.generateReport)
+    public void generateReport(){
+        startActivity(new Intent(AdminNavigationActivity.this, Admin_email_activity.class));
+    }
+
+    @OnClick(R.id.cookButton)
+    public void cookButton(){
+        startActivity(new Intent(AdminNavigationActivity.this, CookActivity.class));
+    }
+
+    @OnClick(R.id.editUser)
+    public void editUser(){
+        startActivity(new Intent(AdminNavigationActivity.this, AdminEditUserActivity.class));
+    }
+
+    @OnClick(R.id.waiterButton)
+    public void waiterButton(){
+        startActivity(new Intent(AdminNavigationActivity.this, TableActivity.class));
+    }
+
+    @OnClick(R.id.editMenu)
+    public void editMenu(){
+        startActivity(new Intent(AdminNavigationActivity.this, AdminEditMenuActivity.class));
+    }
 }
