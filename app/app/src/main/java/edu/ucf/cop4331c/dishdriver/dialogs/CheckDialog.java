a35d7ceb5121553270c0e0d54cda0dbf390767f5
Vivienne Do <viviennedo94@gmail.com>
2017-04-05 22:09:24 -0400

    Added functional 0.18 gratuity feature with button.

--- app/app/src/main/java/edu/ucf/cop4331c/dishdriver/dialogs/CheckDialog.java
+++ app/app/src/main/java/edu/ucf/cop4331c/dishdriver/dialogs/CheckDialog.java
@@ -40,6 +40,7 @@ import static android.net.wifi.WpsInfo.INVALID;
 public class CheckDialog extends DialogFragment {
 
     private static String total;
+    private static String gratuity;
 
     //TODO: Eventually you will want to change this to an arraylist of the item models and not just strings.
 
@@ -60,6 +61,7 @@ public class CheckDialog extends DialogFragment {
         ArrayList<DishModel> convertToStr = new ArrayList<>();
 
         total = getCheckTotalAmount(items);
+        gratuity = getTipAmount(items);
 
 
         convertToStr.addAll(items);
@@ -115,8 +117,10 @@ public class CheckDialog extends DialogFragment {
         Button mTipButton = (Button) view.findViewById(R.id.tipButton);
         Button mSubmitButton = (Button) view.findViewById(R.id.submitButton);
         TextView mItemTotalTextView = (TextView) view.findViewById(R.id.itemTotalTextView);
+        TextView mGratuityTextView = (TextView) view.findViewById(R.id.gratuityTextView);
 
         mItemTotalTextView.setText(total);
+
 
 
 
@@ -128,6 +132,7 @@ public class CheckDialog extends DialogFragment {
             @Override
             public void onClick(View v) {
                 Toaster.toast("hello this button works");
+                mGratuityTextView.setText((String) gratuity);
 
             }
         });
@@ -160,6 +165,29 @@ public class CheckDialog extends DialogFragment {
 
         return MoneyFormatter.format(sum);
     }
+
+    public static String getTipAmount(ArrayList<DishModel> items) {
+
+        // Iterate through item models and sum the price.
+        int sum = 0;
+
+        for (int i = 0; i < items.size(); i++ ) {
+
+            sum += items.get(i).getPrice();
+        }
+
+
+        return String.valueOf((sum * 0.0018));
+
+    }
+
+//    public String getSubtotal() {
+//
+//        if(true) // if gratuityEditTextView is populated
+//            return ( getCheckTotalAmount() + getTipAmount() );
+//
+//        else return getCheckTotalAmount();
+//    }
 
 
 }
